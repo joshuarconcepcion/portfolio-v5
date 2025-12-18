3f56c6a0efda2c4f178d4cd4a518aa0cfaa225fc
Joshua Concepcion <joshuarconcepcion@gmail.com>
2025-12-17 16:42:29 -0800

    WIP tag fixed

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -10,7 +10,7 @@ export default function Homepage(): JSX.Element {
                 <div className='flex flex-col gap-8'>
                     <div className='flex justify-start items-start md:gap-20 gap-16 pt-12 pl-12 md:pl-6'>
                         <h1 className='font-poppins text-[25px] md:text-[40px] whitespace-nowrap'>/*</h1>
-                        <h1 className='font-poppins text-[25px] md:text-[40px]'>JOSHUA <br /> CONCEPCION <br> *Site In-Progress* </br></h1>
+                        <h1 className='font-poppins text-[25px] md:text-[40px]'>JOSHUA <br /> CONCEPCION <br /> *Site In-Progress*</h1>
                     </div>
                     <div className='flex justify-start items-start md:gap-24 gap-14 pt-12 pl-12 md:pl-6'>
                         <h1 className='font-ltsoul text-[26px] md:text-[20px]'>1.1 <br /> 1.2 <br /> 1.3 <br /> 1.4 <br /> 1.5 <br /> 1.6</h1>
